2467f59e4e6b5457d74ef816a9e3a935495cb1a7
Sergi <sergicurero@gmail.com>
2022-11-24 13:54:13 +0100

    millores codi
    Millores del codi per adequar-lo al que demana el professor

--- Classe/Activitat 2/4-5.cpp
+++ Classe/Activitat 2/4-5.cpp
@@ -1,48 +1,14 @@
 #include <iostream>
-
+ 
 int main()
 {
-    //usem un for loop per no haver d'iniciar el programa 4 cops
-    for (int i = 0; i < 4; i++)
-    {
-        //com no podem escanejar un valor booleà, hem d'escanejar un valor enter i assignar-lo a una variable booleana
-        //a_ es refereix a que és una variable auxiliar 
-        bool bonTemps, socFelic, esPerfecte;
-        int a_bonTemps, a_socFelic, a_esPerfecte;
-        printf("Fa bon temps? ");
-        scanf("%i",&a_bonTemps);
-        bonTemps = a_bonTemps;
-        printf("Ets feliç? ");
-        scanf("%i",&a_socFelic);
-        socFelic = a_socFelic;
-
-        /*
-        Volem fer una porta AND
-        Taula de la veritat per una porta AND:
-        0+0=0
-        0+1=0
-        1+0=0
-        1+1=1
-
-        if vol dir "si" (condicional) en angles, exemple: si fa sol, anire a la platja
-        else vol dir "d'altra banda". Si no hem entrat al loop if, entrarem al loop else
-        */
-
-        //en aquest cas usem el loop if per:
-        //si bonTemps i socFelic son true (1), entrem al loop if
-        if (bonTemps && socFelic)
-        {
-            //assignem el valor a una variable en cas de voler usar-la
-            esPerfecte = 1;
-            printf("El dia és perfecte :D\n");
-        }
-        //si alguna de les dues o les dues son false (0), no entrem a if i entrem al loop else
-        else
-        {
-            esPerfecte = 0;
-            printf("El dia no és perfecte :c\n");
-        }
-    }
-    
+    int bonTemps, socFelic;
+    printf("Fa bon temps? ");
+    scanf("%i",&bonTemps);
+    printf("Ets feliç? ");
+    scanf("%i",&socFelic);
+ 
+    printf("El dia és perfecte (%i)\n", bonTemps&&socFelic);
+ 
     return 0;
 }
--- Classe/Activitat 3/3.cpp
+++ Classe/Activitat 3/3.cpp
@@ -1,16 +1,7 @@
-/*
-El programa  ens demana un número n (número de alumnes).
-Cada alumne ens introdueix els seus diners (euros) de butxaca.
-Al final del programa mostra per pantalla la suma total de euros. 
-Al final del programa has de demanar “Vols sortir del programa?”. 
-Només si introduïm ‘s’ o ‘S’ sortim del programa.
-La suma total no es va acumulant.
-*/
 #include <iostream>
 using namespace std;
 int main()
 {   
-    //bool sortir = false;
     char resposta;
     int alumnes;
     float euros, suma = 0;
@@ -20,11 +11,11 @@ int main()
         scanf("%i",&alumnes);
         for (int i = 0; i < alumnes; i++)
         {
-            printf("Alumne %i introdueix els teus diners: ", i+1);
+            printf("Alumne %i introdueix els teus diners (€): ", i+1); //i++ no funciona i fa que el for augmenti de 2 en 2
             scanf("%f", &euros);
             suma+=euros;
         }
-        printf("Suma total: %f\n", suma);
+        printf("Suma total: %f€\n", suma);
         printf("Desitja sortir del programa? ");
         scanf("%s", &resposta);
         if (resposta == 's' || resposta == 'S')
